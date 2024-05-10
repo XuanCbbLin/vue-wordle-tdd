7d27f0276fc50de1d1e98a567ad3ce7d307dd672
Xuan <98876600+XuanCbbLin@users.noreply.github.com>
2024-05-10 11:41:28 +0800

    feat: Make player guesses case-insensitive

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -18,7 +18,7 @@ const formattedGuessesInProgress = computed({
     return guessInProgress.value
   },
   set(rawValue: string) {
-    guessInProgress.value = rawValue.slice(0, WORD_SIZE)
+    guessInProgress.value = rawValue.slice(0, WORD_SIZE).toLocaleUpperCase()
   }
 })
 
