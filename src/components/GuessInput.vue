5cf95a9317234a32c4738d653e026080e7822648
Xuan <98876600+XuanCbbLin@users.noreply.github.com>
2024-05-18 14:43:58 +0800

    refactor: Extract GuessView component

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -2,6 +2,7 @@
 import { WORD_SIZE } from '@/utils/message'
 import englishWords from '@/data/englishWordsWith5Letters.json'
 import { computed, ref, triggerRef } from 'vue'
+import GuessView from './GuessView.vue'
 
 const emit = defineEmits<{
   'guess-submitted': [guess: string]
@@ -57,16 +58,7 @@ function onSubmit() {
 </script>
 
 <template>
-  <ul class="word">
-    <li
-      v-for="(letter, index) in formattedGuessesInProgress.padEnd(WORD_SIZE, ' ')"
-      :key="`${letter}-${index}`"
-      :data-letter="letter"
-      class="letter"
-    >
-      {{ letter }}
-    </li>
-  </ul>
+  <GuessView :guess="formattedGuessesInProgress" />
 
   <input
     type="text"
@@ -79,59 +71,8 @@ function onSubmit() {
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-
 input {
   position: absolute;
   opacity: 0;
-}
-.word {
-  list-style: none;
-  padding: 0;
-  display: flex;
-  gap: 0.25rem;
-}
-.letter {
-  background-color: white;
-  border: 1px solid hsl(0, 0%, 70%);
-  width: 5rem;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  font-weight: bolder;
-}
-li:not([data-letter=' ']) {
-  animation: pop 100ms;
-}
-@keyframes pop {
-  0% {
-    transform: scale(1);
-  }
-  50% {
-    transform: scale(1.4);
-  }
 }
 </style>
---
+++ src/components/GuessView.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { WORD_SIZE } from '@/utils/message'
+
+defineProps<{ guess: string }>()
+</script>
+
+<template>
+  <ul class="word">
+    <li
+      v-for="(letter, index) in guess.padEnd(WORD_SIZE, ' ')"
+      :key="`${letter}-${index}`"
+      :data-letter="letter"
+      class="letter"
+    >
+      {{ letter }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+.word {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.25rem;
+}
+.letter {
+  background-color: white;
+  border: 1px solid hsl(0, 0%, 70%);
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bolder;
+}
+li:not([data-letter=' ']) {
+  animation: pop 100ms;
+}
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.4);
+  }
+}
+</style>
